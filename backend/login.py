610f668dda656c104619e7cc1f61e39a04ad1a10
Setsu_ <BobssoR321@gmail.com>
2026-05-10 21:10:34 +0200

    Update login.py

--- backend/login.py
+++ backend/login.py
@@ -7,9 +7,10 @@ from auth import create_access_token
 from security import verify_password
 
 def login(name: str, password: str, db: Session = Depends(get_db)):
+    #user als Variable aus Datenbank entnehmen
     user = db.query(Admin).filter(Admin.name == name).first()
 
-    # Passwort mit bcrypt prüfen
+    #Schauen ob user existiert und Passwort prüfen
     if not user or not verify_password(password, user.password):
         raise HTTPException(status_code=401, detail="Falsche Login-Daten")
 
