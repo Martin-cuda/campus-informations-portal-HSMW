8289b8443e1b2be8bb423707c177ac1a54fd61e4
Setsu_ <BobssoR321@gmail.com>
2026-05-10 20:56:44 +0200

    Add files via upload

---
+++ backend/login.py
@@ -0,0 +1,21 @@
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
+from datenbank import get_db
+from fastapi import FastAPI
+from admin_table import Admin
+from auth import create_access_token
+from security import verify_password
+
+def login(name: str, password: str, db: Session = Depends(get_db)):
+    user = db.query(Admin).filter(Admin.name == name).first()
+
+    # Passwort mit bcrypt prüfen
+    if not user or not verify_password(password, user.password):
+        raise HTTPException(status_code=401, detail="Falsche Login-Daten")
+
+    token = create_access_token({"sub": user.name})
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
