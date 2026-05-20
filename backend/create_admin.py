63dc3dcfe7bcab62a393a42193df64c044a29d44
Setsu_ <BobssoR321@gmail.com>
2026-05-20 17:36:54 +0200

    main admin hinzufügen + extra mail eintrag

--- backend/create_admin.py
+++ backend/create_admin.py
@@ -1,20 +1,43 @@
 from sqlalchemy.orm import Session
 from datenbank import SessionLocal
 from admin_table import Admin
-from module import generate_default_permission
+from module import generate_default_permission, generate_main_permission
 import json
+from security import hash_password
 
 
 
-def create_admin(name: str, password: str):
+def create_admin(name: str, password: str, mail: str):
     db: Session = SessionLocal()
     default_permissions = {}
     try:
         new_admin = Admin(name = name, 
-                          password = password, 
+                          password = hash_password(password), 
                           role = "secondary_admin", 
                           #alle rechte von Modulen werden erstmal als "False" deklariert
-                          permissions = json.dumps(generate_default_permission()))
+                          permissions = json.dumps(generate_default_permission()),
+                          mail = mail)
+        db.add(new_admin)
+        db.commit()
+        db.refresh(new_admin)
+        print(f"Admin angelegt: {new_admin.id} - {new_admin.name}")
+    except Exception as e:
+        db.rollback()
+        print("Fehler:", e)
+    finally:
+        db.close()
+
+
+def create_admin_main(name: str, password: str, mail: str):
+    db: Session = SessionLocal()
+    default_permissions = {}
+    try:
+        new_admin = Admin(name = name, 
+                          password = hash_password(password), 
+                          role = "primary_admin", 
+                          #alle rechte von Modulen werden erstmal als "False" deklariert
+                          permissions = json.dumps(generate_main_permission()),
+                          mail = mail)
         db.add(new_admin)
         db.commit()
         db.refresh(new_admin)
@@ -26,4 +49,8 @@ def create_admin(name: str, password: str):
         db.close()
 
 if __name__ == "__main__":
-    create_admin("Journalist", "123abc") 
+    create_admin_main("HauptAdmin", "hsmwPORTAL", "Test-CampusPortalMailStudent123@web.de")
+    create_admin("Journalist", "123abc", "Test-CampusPortalMailStudent123@web.de") 
+    create_admin("Journalist", "InfoHSMW", "Test-CampusPortalMailStudent123@web.de")
+    create_admin("Mensa", "essenHSMW", "Test-CampusPortalMailStudent123@web.de")
+    create_admin("Haeuser", "HausHSMW", "Test-CampusPortalMailStudent123@web.de")
