c2700b6ef418c02bdbe857ab6df6586c352f367c
Setsu_ <BobssoR321@gmail.com>
2026-05-20 17:38:02 +0200

    neue "Mail" Spalte für Admins

--- backend/admin_table.py
+++ backend/admin_table.py
@@ -9,6 +9,6 @@ class Admin(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
     password = Column(String)
+    mail = Column(String)
     permissions = Column(String) #als JSON für Klappleiste
     role = Column(String, default="secondary_admin") #neuer erstellter Admin ist automatisch secondary
-
